345e31841317961ef210e966103e6ea4c5c40cc1
Denys <denys.hula@gmail.com>
2024-09-24 17:13:24 +0300

    Added the game field, added the bat-files to start game and server, added the server target

---
+++ StartGame.bat
@@ -0,0 +1,2 @@
+start "Pong Game" "C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\Win64\UnrealEditor.exe" ^
+"%~dp0Pong.uproject" -game -log -ResX=854 -ResY=480 -windowed
---
+++ StartServer.bat
@@ -0,0 +1,2 @@
+start "Pong Server" "C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\Win64\UnrealEditor.exe" ^
+"%~dp0Pong.uproject" -server -log -port=7776
